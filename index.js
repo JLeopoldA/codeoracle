564a1c447a66bbf9518a2078afe36fbe908d6f23
Justin Acosta <>
2023-05-17 04:32:15 -0400

    fetch post included

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
+
+
+
+// FETCH POST FUNCTION
+async function postQuestion(inquiry) {
+    try {
+        const data = {
+            question: inquiry
+        }
+        const response = await fetch("https://localhost:3000/api/request", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+// PARAMETER SELECTION
 const selection = process.argv[2];
 const question = process.argv.slice(3).join(' ');
-
-const commandQuestion = ["q", "-q", "--q", "-question", "--question", "question", 
+const parameterQuestion = ["q", "-q", "--q", "-question", "--question", "question", 
                         "a", "-a", "--a", "-ask", "--ask", "ask" ];
-const commandHelp = ["h", "-h", "--h", "-help", "--help", "help"];
-function matchSelection(commandArr, selectedCommand) {
-    for(let i = 0; i < commandArr.length; i++) {
-        if(commandArr[i] == selectedCommand) {
+const parameterHelp = ["h", "-h", "--h", "-help", "--help", "help"];
+function matchSelection(parameterArr, selectedParameter) {
+    for(let i = 0; i < parameterArr.length; i++) {
+        if(parameterArr[i] == selectedParameter) {
             return true;
         }
     }
 };
 
+// RUN FUNCTION
 function run() {
-    if (matchSelection(commandQuestion, selection)) { // if user has asked question
-        console.log(question);
-    } else if (matchSelection(commandHelp, selection)) { // if user is requesting help
+    if (matchSelection(parameterQuestion, selection)) { // if user has asked question
+        postQuestion(question);
+    } else if (matchSelection(parameterHelp, selection)) { // if user is requesting help
         console.log("help requested");
     } else { // if no parameter is selected
         console.log("nothing has been selected");
